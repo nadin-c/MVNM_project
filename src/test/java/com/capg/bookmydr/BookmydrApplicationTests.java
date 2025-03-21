ab2bddae7faa48c439fb25eb2bfed76764d710db
NADIN C <cnadin005@gmail.com>
2025-03-21 10:07:03 +0530

    Update BookmydrApplicationTests.java

--- src/test/java/com/capg/bookmydr/BookmydrApplicationTests.java
+++ src/test/java/com/capg/bookmydr/BookmydrApplicationTests.java
@@ -2,11 +2,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-@ContextConfiguration(classes = BookmydrApplication.class)  // Specify the main application class
+@ContextConfiguration(classes = com.capg.bookmydr.BookmydrApplication.class)  // Specify the main application class
 @SpringBootTest
 public class BookmydrApplicationTests {
-
     @Test
     void contextLoads() {
         assertTrue(true);  // Dummy assertion to validate the context loading
