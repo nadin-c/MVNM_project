1a775911722010e7ab6aed5ba148f557518abe21
NADIN C <cnadin005@gmail.com>
2025-03-21 10:10:15 +0530

    Update BookmydrApplicationTests.java

--- src/test/java/com/capg/bookmydr/BookmydrApplicationTests.java
+++ src/test/java/com/capg/bookmydr/BookmydrApplicationTests.java
@@ -2,7 +2,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-@ContextConfiguration(classes = com.capg.bookmydr.BookmydrApplication.class)  // Specify the main application class
+@ContextConfiguration(classes = src.test.java.com.capg.bookmydr.BookmydrApplication.class)  // Specify the main application class
 @SpringBootTest
 public class BookmydrApplicationTests {
     @Test
