7d9a148c53e4ab9be1ace8687e123a57cd6aa0ab
NADIN C <cnadin005@gmail.com>
2025-03-21 09:58:58 +0530

    Update BookmydrApplication.java

--- src/main/java/com/spring/bookmydr/BookmydrApplication.java
+++ src/main/java/com/spring/bookmydr/BookmydrApplication.java
@@ -3,7 +3,9 @@ package com.spring.bookmydr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
 @ComponentScan("com.spring.bookmydr")
+
 @SpringBootApplication
 public class BookmydrApplication {
 
